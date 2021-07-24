34e763cd0d5ca87c6ab1f14f49d7a1579b594ea1
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-07-24 22:24:06 +0300

    add funcion to hide and show div on click on what we do section

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+  $("#designImg").click(function () {
+    $("#design").show();
+    $("#designImg").hide();
+  });
+  $("#design").click(function () {
+    $("#designImg").show();
+    $("#design").hide();
+  });
+  $("#developmentImg").click(function () {
+    $("#development").show();
+    $("#developmentImg").hide();
+  });
+  $("#development").click(function () {
+    $("#developmentImg").show();
+    $("#development").hide();
+  });
+  $("#productionImg").click(function () {
+    $("#production").show();
+    $("#productionImg").hide();
+  });
+  $("#production").click(function () {
+    $("#productionImg").show();
+    $("#production").hide();
+  });
+});
