96e5a4101516fd4167512af35bc190bd35a9ee26
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-07-25 22:44:25 +0300

    add jquery to enable hover effect of portfolio section

--- js/script.js
+++ js/script.js
@@ -1,26 +1,85 @@
 $(document).ready(function () {
   $("#designImg").click(function () {
-    $("#design").show();
+    $("#designInfo").show();
     $("#designImg").hide();
   });
-  $("#design").click(function () {
+  $("#designInfo").click(function () {
     $("#designImg").show();
-    $("#design").hide();
+    $("#designInfo").hide();
   });
   $("#developmentImg").click(function () {
-    $("#development").show();
+    $("#developmentInfo").show();
     $("#developmentImg").hide();
   });
-  $("#development").click(function () {
+  $("#developmentInfo").click(function () {
     $("#developmentImg").show();
-    $("#development").hide();
+    $("#developmentInfo").hide();
   });
   $("#productionImg").click(function () {
-    $("#production").show();
+    $("#productionInfo").show();
     $("#productionImg").hide();
   });
-  $("#production").click(function () {
+  $("#productionInfo").click(function () {
     $("#productionImg").show();
-    $("#production").hide();
+    $("#productionInfo").hide();
   });
+});
+
+$(document).ready(function () {
+  $("#portfolio1")
+    .mouseenter(function () {
+      $("#overlay1").show();
+    })
+    .mouseleave(function () {
+      $("#overlay1").hide();
+    });
+  $("#portfolio2")
+    .mouseenter(function () {
+      $("#overlay2").show();
+    })
+    .mouseleave(function () {
+      $("#overlay2").hide();
+    });
+  $("#portfolio3")
+    .mouseenter(function () {
+      $("#overlay3").show();
+    })
+    .mouseleave(function () {
+      $("#overlay3").hide();
+    });
+  $("#portfolio4")
+    .mouseenter(function () {
+      $("#overlay4").show();
+    })
+    .mouseleave(function () {
+      $("#overlay4").hide();
+    });
+  $("#portfolio5")
+    .mouseenter(function () {
+      $("#overlay5").show();
+    })
+    .mouseleave(function () {
+      $("#overlay5").hide();
+    });
+  $("#portfolio6")
+    .mouseenter(function () {
+      $("#overlay6").show();
+    })
+    .mouseleave(function () {
+      $("#overlay6").hide();
+    });
+  $("#portfolio7")
+    .mouseenter(function () {
+      $("#overlay7").show();
+    })
+    .mouseleave(function () {
+      $("#overlay7").hide();
+    });
+  $("#portfolio8")
+    .mouseenter(function () {
+      $("#overlay8").show();
+    })
+    .mouseleave(function () {
+      $("#overlay8").hide();
+    });
 });
